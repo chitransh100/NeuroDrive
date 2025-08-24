bb8c2933347344e5a88ec7f3396131f07982a0b5
chitransh100 <kumarchitransh12@gmail.com>
2025-08-25 03:34:39 +0530

    completed the complete car movements

--- car.js
+++ car.js
@@ -1,34 +1,102 @@
-class Car{
-    //no need to declare the variables in class 
-    constructor(x,y,width,height){
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
-        //till now there are no controls of the car so create a new object defining the controls 
-        this.controls = new Controls(); 
-        //controls will be the object defining the movement of the car 
+class Car {
+  //no need to declare the variables in class
+  //dealing with 4th qudrant here
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    //let us introduce the speed and the accelerations here
+    this.speed = 0;
+    this.acceleration = 0.2;
+    //introducing friction to stop the car
+    this.friction = 0.05;
+    this.maxSpeed = 3;
+    //introduce an angle
+    this.angle = 0; //from the x axis
+
+    // ---------------------------> x asix
+    // |\  angle from the x-axis
+    // |  \
+    // |    \
+    // |      \
+    // |
+    // y-axis
+
+    //till now there are no controls of the car so create a new object defining the controls
+    this.controls = new Controls();
+    //controls will be the object defining the movement of the car
+  }
+
+  update() {
+    //for up and down
+    if (this.controls.forward) {
+      // this.y -= 2; the y increases downwards -> we need some acceleration here
+      this.speed += this.acceleration; //v=v+a⋅Δt (Δt = 1sec)
+    }
+    if (this.controls.reverse) {
+      // this.y += 1; have to use "this" keyword
+      this.speed -= this.acceleration;
     }
 
-    update(){
-        if(this.controls.forward){
-            this.y -= 2; //the y increases downwards 
-        }
-        if(this.controls.reverse){
-            this.y += 1; //have to use "this" keyword
-        }
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed; //capping the speed
+    }
+    if (this.speed < -this.maxSpeed) {
+      this.speed = -this.maxSpeed;
+      //capping the speed for the opposite direction (- == opposite direction)
+    }
+    if (this.speed > 0) {
+      this.speed -= this.friction; //vnew=vold​×(1−f)
+    }
+    if (this.speed < 0) {
+      //for reverse speed
+      this.speed += this.friction; //friction always opposes motion
+    }
+    // Prevent oscillation near 0 (very important)
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+      //If the current speed is smaller than the friction amount, we just force the speed to 0 (stop the car completely).
+    }
+    //for left and right
+    if (this.speed != 0) { //makes sure that if speed is 0 then do nothing dont move right or left 
+        const flip = this.speed>0 ? 1:-1;
+    
+      if (this.controls.left) {
+        // this.x -= 2;
+        this.angle += 0.03*flip; //moving the line towards teh y axis
+      }
+      if (this.controls.right) {
+        // this.x += 2;
+        this.angle -= 0.03*flip ;
+      }
     }
 
-    draw(ctx){
-        //takes ctx as a input which issues us the drawing commands
-        ctx.beginPath();  //starting new shape (forget previous shapes )
-        ctx.rect(          //creates rectangle path on the given spaces 
-            this.x-this.width/2,
-            this.y-this.height/2,
-            this.width,
-            this.height,
-        );
-        ctx.fillStyle = "blue";   //sets (fills) the car's color
-        ctx.fill(); //fills the rectangle with the current fillStyle (color).
-    }
+    // this.y -= this.speed; //y=y+v⋅Δt (Δt = 1sec)
+    this.y -= Math.cos(this.angle) * this.speed; //speed has 2 components now
+    this.x -= Math.sin(this.angle) * this.speed; //so as the distance will have
+  }
+
+  draw(ctx) {
+    //initially canvas has the origin at the top left corner.
+    ctx.save(); //??????????????????
+    ctx.translate(this.x, this.y); //move origin to the car's (x, y) position
+    ctx.rotate(-this.angle); //rotates the axes counterclockwise by θ around the origin (-ve for the clock wise)
+    //takes ctx as a input which issues us the drawing commands
+    ctx.beginPath(); //starting new shape (forget previous shapes )
+    ctx.rect(
+      //takes the center of rectangle (x,y) and the width
+      //creates rectangle path on the given spaces
+      //   this.x - this.width / 2,
+      //   this.y - this.height / 2,
+      -this.width / 2, //after translate teh center is this now
+      -this.height / 2, //we shifted the origin to x,y
+      this.width,
+      this.height
+    );
+    ctx.fillStyle = "blue"; //sets (fills) the car's color
+    ctx.fill(); //fills the rectangle with the current fillStyle (color).
+    ctx.restore(); //????????????????
+    //With save/restore: each shape can be drawn in its own local coordinate system.
+  }
 }
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("gameCanvas")  //refrencing the canvas we defined in the html 
-canvas.height = window.innerHeight;
+// canvas.height = window.innerHeight;
 canvas.width = 200;
 //  <canvas> element used for drawing graphics on a web page, the context refers to the specific rendering API or environment that provides the methods and properties for performing drawing operations.
 // Essentially, the <canvas> element itself acts as the drawing surface, while the context is the "toolset" you use to actually draw on that surface.
@@ -11,7 +11,22 @@ car.draw(ctx) //a methd in the car class
 animate();
 
 function animate(){
-    car.update();
-    car.draw(ctx);
-    requestAnimationFrame(animate);
+    car.update(); //another funtion which runs when the arrowkeys are triggered 
+    canvas.height = window.innerHeight; //putting this here to handle the trail effect (wipes the canvas in every frame);
+    car.draw(ctx); //function in the car.js
+    requestAnimationFrame(animate); //browser-provided function that you use to create smooth animations in JavaScript. Instead of using setInterval or setTimeout to run your animation loop (which can cause stuttering), you use requestAnimationFrame(). The browser then calls your function just before the next repaint, giving you the smoothest and most efficient update.
+
 }
+
+//NOTES
+// inside the animation loop (before drawing), you are resetting the entire canvas each frame.
+// That does two things:
+// Clears the canvas completely (like wiping a whiteboard before drawing again).
+// Resets it to the full browser window height (so you always see the full drawing area, no shrinking).
+// So now, on each frame:
+// Old drawings are erased.
+// Only the new position of the car is drawn.
+// This creates the illusion of smooth movement.
+//  Instead of re-setting height every frame (which is a bit heavy-handed), some people use:
+// ctx.clearRect(0, 0, canvas.width, canvas.height);
+// That just clears the canvas without resizing it
